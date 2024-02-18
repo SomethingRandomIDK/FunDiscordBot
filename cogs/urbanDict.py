41fb73056a588626b8ff850d294a2af594131aa8
Mehul <mehtomar@gmail.com>
2024-02-17 23:08:05 -0500

    Can now search urban dictionary by work and scroll through top 10 defintions of that word

--- cogs/urbanDict.py
+++ cogs/urbanDict.py
@@ -5,6 +5,9 @@ class UrbanDict(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.randList = []
+        self.selDefList = []
+        self.defListLen = 0
+        self.selWord = None
 
     def generateResponse(self, wordEntry, word=None):
         response = ''
@@ -27,4 +30,33 @@ class UrbanDict(commands.Cog):
         message = self.generateResponse(self.randList[0])
         await ctx.send(message)
         self.randList.pop(0)
+
+    @commands.command(name='urban')
+    async def selectedWord(self, ctx, *, arg):
+        url = 'https://api.urbandictionary.com/v0/define?term='
+        url += arg.lower().strip().replace(' ', '&')
+
+        self.selDefList = requests.get(url).json()['list']
+        self.defListLen = len(self.selDefList)
+        self.selWord = arg
+        if self.defListLen == 0:
+            await ctx.send(f'{arg} was not found in the Urban Dictionary')
+            return
+
+        message = f'*Defintion 1 of {self.defListLen}*\n'
+        message += self.generateResponse(self.selDefList[0], arg)
+        await ctx.send(message)
+        self.selDefList.pop(0)
+
+    @commands.command(name='nextdef')
+    async def getNextDefintion(self, ctx):
+        if len(self.selDefList) == 0:
+            await ctx.send(f'There are no additional definitions available please check [the official Urban Dictionary website](https://www.urbandictionary.com) for more defintions')
+            return
+
+        curDef = self.defListLen - len(self.selDefList) + 1
+        message = f'*Definition {curDef} of {self.defListLen}*\n'
+        message += self.generateResponse(self.selDefList[0], self.selWord)
+        await ctx.send(message)
+        self.selDefList.pop(0)
 
