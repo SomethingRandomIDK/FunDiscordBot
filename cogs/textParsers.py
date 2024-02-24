bf31a822feaac867240bfa261e14d700e005886f
Mehul <mehtomar@gmail.com>
2024-02-23 23:24:47 -0500

    taking break

--- cogs/textParsers.py
+++ cogs/textParsers.py
@@ -17,39 +17,48 @@ class TextParsers(commands.Cog):
         #
         # if len(formattedContent) == len(pTableString):
         #     return pTableString
-        pTable = {'h':['', 'e', 'f', 'g', 'o', 's'],
-                  'l':['a', 'i', 'r', 'u', 'v'],
-                  'b':['', 'a', 'e', 'h', 'i', 'k', 'r'],
-                  'c':['', 'a', 'd', 'e', 'f', 'l', 'm', 'n', 'o', 'r', 's', 'u'],
-                  'n':['', 'a', 'b', 'd', 'e', 'h', 'i', 'o', 'p'],
-                  'o':[''. 'g', 's'],
-                  'f':['', 'e', 'l', 'm', 'r'],
-                  'm':['c', 'd', 'g', 'n', 'o', 't'],
-                  'a':['c', 'g', 'l', 'm', 'r', 's', 't', 'u'],
-                  's':['', 'b', 'c', 'e', 'g', 'i', 'm', 'n', 'r'],
-                  'p':['', 'a', 'b', 'd', 'm', 'o', 'r', 't', 'u'],
-                  'k':['', 'r'],
-                  't':['a', 'b', 'c', 'e', 'h', 'i', 'l', 'm', 's'],
-                  'v':[''],
-                  'z':['n', 'r'],
-                  'g':['a', 'd', 'e'],
-                  'r':['a', 'b', 'e', 'f', 'g', 'h', 'n', 'u'],
-                  'y':['', 'b'],
-                  'i':['', 'n', 'r'],
-                  'x':['e'],
-                  'w':[''],
-                  'd':['b', 's', 'y'],
-                  'e':['s', 'r', 'y'],
-                  'u':['']}
+        pTable = {'h': ['', 'e', 'f', 'g', 'o', 's'],
+                  'l': ['a', 'i', 'r', 'u', 'v'],
+                  'b': ['', 'a', 'e', 'h', 'i', 'k', 'r'],
+                  'c': ['', 'a', 'd', 'e', 'f', 'l', 'm', 'n', 'o', 'r', 's', 'u'],
+                  'n': ['', 'a', 'b', 'd', 'e', 'h', 'i', 'o', 'p'],
+                  'o': ['', 'g', 's'],
+                  'f': ['', 'e', 'l', 'm', 'r'],
+                  'm': ['c', 'd', 'g', 'n', 'o', 't'],
+                  'a': ['c', 'g', 'l', 'm', 'r', 's', 't', 'u'],
+                  's': ['', 'b', 'c', 'e', 'g', 'i', 'm', 'n', 'r'],
+                  'p': ['', 'a', 'b', 'd', 'm', 'o', 'r', 't', 'u'],
+                  'k': ['', 'r'],
+                  't': ['a', 'b', 'c', 'e', 'h', 'i', 'l', 'm', 's'],
+                  'v': [''],
+                  'z': ['n', 'r'],
+                  'g': ['a', 'd', 'e'],
+                  'r': ['a', 'b', 'e', 'f', 'g', 'h', 'n', 'u'],
+                  'y': ['', 'b'],
+                  'i': ['', 'n', 'r'],
+                  'x': ['e'],
+                  'w': [''],
+                  'd': ['b', 's', 'y'],
+                  'e': ['s', 'r', 'y'],
+                  'u': ['']}
+        # Need to try a back tracking algorithm I think
 
-        pass
+        formattedContent = ''.join([x.lower() for x in content if x.lower() in 'abcdefghijklmnopqrstuvwxyz'])
+        if len(formattedContent) == 0 or not pTable[formattedContent[0]]:
+            return
+
+        pTableRewrite = []
+        idx = 0
+        while idx < len(formattedContent):
+            pass
+        return pTableRewrite
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
         pTableCheck = self.searchPeriodicTable(message.content)
-        if pTableCheck:
+        if len(pTableCheck) != 0:
             pTableResponse = 'Your message can be rewritten using Elements from the Periodic Table as:\n'
             pTableResponse += pTableCheck
 
