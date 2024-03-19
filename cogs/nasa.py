617bcfa745a2ba3d79cec30dbb97666a0668fd7a
Mehul <mehtomar@gmail.com>
2024-03-19 13:42:23 -0400

    Cleaned the code for the Nasa funciton and allowed users to go to an alternate image

--- cogs/nasa.py
+++ cogs/nasa.py
@@ -8,6 +8,7 @@ class Nasa(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.color = 0x7aa9f5
+        self.searchResults = None
 
     @commands.command(name='nasaSearch')
     async def nasaSearch(self, ctx, *args):
@@ -15,7 +16,20 @@ class Nasa(commands.Cog):
         searchParams = {'q': search,
                         'media_type': 'image'}
         images = requests.get(urlSearch, params=searchParams).json()
+        self.searchResults = images['collection']['items']
         embed = discord.Embed(color=self.color, title='NASA Image Search')
-        embed.set_image(url=images['collection']['items'][0]['links'][0]['href'])
+        embed.set_image(url=self.searchResults[0]['links'][0]['href'])
+        embed.add_field(name='Image Title:',
+                        value=self.searchResults[0]['data'][0]['title'])
         await ctx.send(embed=embed)
+        self.searchResults.pop(0)
+
+    @commands.command(name='nextImg')
+    async def nextImg(self, ctx):
+        embed = discord.Embed(color=self.color, title='NASA Image Search')
+        embed.set_image(url=self.searchResults[0]['links'][0]['href'])
+        embed.add_field(name='Image Title:',
+                        value=self.searchResults[0]['data'][0]['title'])
+        await ctx.send(embed=embed)
+        self.searchResults.pop(0)
 
